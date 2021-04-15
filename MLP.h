01548ee5c7a0f489d2e4dba32065226c112f1a0a
Tom Bertalan <tom@tombertalan.com>
2021-04-15 13:51:04 -0400

    Do a more interesting example, and clumsily plot it.

--- MLP.h
+++ MLP.h
@@ -2,32 +2,44 @@
         #ifndef MLP_H
         #define MLP_H
         int kernel_0_shape[2];
-double kernel_0[6];
+double kernel_0[10];
 
 int bias_0_shape[2];
-double bias_0[3];
+double bias_0[10];
 
 int xA_0_shape[2];
-double xA_0[12];
+double xA_0[1280];
 
 int preactivation_1_shape[2];
-double preactivation_1[12];
+double preactivation_1[1280];
 
 int kernel_1_shape[2];
-double kernel_1[3];
+double kernel_1[100];
 
 int bias_1_shape[2];
-double bias_1[1];
+double bias_1[10];
 
 int xA_1_shape[2];
-double xA_1[4];
+double xA_1[1280];
+
+int preactivation_2_shape[2];
+double preactivation_2[1280];
+
+int kernel_2_shape[2];
+double kernel_2[10];
+
+int bias_2_shape[2];
+double bias_2[1];
+
+int xA_2_shape[2];
+double xA_2[128];
 
 void setup();
 
         
         void MLP(
             const double *preactivation_0,
-            double *preactivation_2
+            double *preactivation_3
         );
         
         #endif //MLP_H
--- keras_cmodel.py
+++ keras_cmodel.py
@@ -223,26 +223,63 @@ class CModel(CCode):
 
 
 def test():
+    from subprocess import check_output
+    import matplotlib.pyplot as plt
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(3, activation='tanh'),
+        tf.keras.layers.Dense(10, activation='tanh'),
+        tf.keras.layers.Dense(10, activation='tanh'),
         tf.keras.layers.Dense(1, activation='linear'),
     ])
     model.compile(optimizer=tf.keras.optimizers.Adam(
-        learning_rate=1e-4), loss='mse')
+        learning_rate=1e-3), loss='mse')
 
-    model.fit(
-        np.random.uniform(size=(1000, 2)),
-        np.random.uniform(size=(1000, 1))
-    )
+    x = np.random.uniform(size=(1000, 1), low=-1, high=1)
+    true_func = lambda x: x ** 2
+    y = true_func(x)
+    history = model.fit(x, y, 
+        epochs=500, verbose=0, batch_size=500,
+    ).history
+    fig, ax = plt.subplots()
+    ax.plot(history['loss'])
+    ax.set_yscale('log')
+    ax.set_ylabel('Loss')
+    ax.set_xlabel('Batch')
+    fig.savefig('mlp_training.png')
 
-    cmodel = CModel(model, batch_size=4)
-    print('CODE:')
-    print(cmodel.code)
-
-    print('\n\n\nHEADER:')
-    print(cmodel.header)
-
+    test_batch_size=128
+    cmodel = CModel(model, batch_size=test_batch_size)
     cmodel.save()
+    x_test =  np.linspace(-1, 1, test_batch_size)
+    inputs = ','.join([str(f) for f in x_test])
+
+    test_program = '''#include "MLP.h"
+#include "mm_utils.h"
+
+int main() {{
+    setup();
+    double inputs[] = {{ {inputs} }};
+    double outputs[{batchsize}];
+    MLP(inputs, outputs);
+    print_array("outputs", outputs, {batchsize}, 1);
+}}'''.format(
+        inputs=inputs,
+        batchsize=test_batch_size,
+    )
+    with open('MLP_test.c', 'w') as fp:
+        fp.write(test_program)
+
+    build_result = check_output('gcc MLP_test.c MLP.c mm_utils.c -lm -g -o MLP_test'.split())
+    result = check_output(['./MLP_test']).decode('utf-8')
+    with open('MLP_result.py', 'w') as fp:
+        fp.write(result)
+    from MLP_result import outputs
+    outputs = np.array(outputs)
+
+    fig, ax = plt.subplots()
+    ax.plot(x_test, true_func(x_test), label='Truth')
+    ax.scatter(x_test, outputs.ravel(), label='Net')
+    ax.legend()
+    fig.savefig('mlp_result.png')
 
 
 if __name__ == '__main__':
